bd0ac050c34c096d17f4c620ecf5b632d83b8035
DeepakS-Github <deepakanonymous75@gmail.com>
2023-07-28 02:31:46 +0530

    Added

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,13 @@ const blogRoute = require('./routes/Blogs');
 const PORT = 4500;
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern.app"],
+        methods: ["POST","GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 app.use('/', blogRoute);
 
